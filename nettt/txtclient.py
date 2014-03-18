caa90fd8cff42dfac1300267ef9740fb3e54fd22
Maja Frydrychowicz <mjzffr@gmail.com>
2014-03-18 15:33:43 -0400

    basic commands to parse

--- nettt/txtclient.py
+++ nettt/txtclient.py
@@ -1,23 +1,36 @@
 import ttt
 import socket
 import errno
+import sys
 
 HOST = 'localhost'
 PORT = 50000
-
 GSTATES = ttt.GSTATES
 BSTATES = ttt.BSTATES
+
+
 # initalize game after connection?
-game = ttt.TicTacToeGame()
+#game = ttt.TicTacToeGame()
 # connected?
 # do I have a partner?
 # what if partner disconnects?
 # which player am i?
 
-class TTTClient:
+class TTTClient(object):
     def __init__(self):
         self.s = None
         self.reset_connection()
+
+    def reset_connection(self):
+        ''' Called at beginning and when peer disconnects. '''
+        if self.s:
+            self.s.close()
+        self.s = socket.socket()
+        self.s.settimeout(30)
+        self.connected = False
+        self.partner = None
+        # TODO how will GUI know that self.connected is False? Poll for
+        # value with after every time local player makes a move?
 
     def connect(self, attempts=10):
         if self.connected:
@@ -41,7 +54,6 @@ class TTTClient:
                 TTTClient.raise_surprise(e,[errno.ECONNABORTED,
                                            errno.ECONNREFUSED])
 
-
     @staticmethod
     def raise_surprise(e, errcodes):
         ''' Propagates exception e if it doesn't match any errcodes '''
@@ -59,42 +71,90 @@ class TTTClient:
         self.partner = self.s.recv(8)
             #self.role = self.s.recv(8)
 
+    def ask_status(self):
+        pass
 
     def send_move(self):
         pass
 
-    def send_forfeit(self):
+    def send_newgame(self):
         pass
 
     def end_session(self):
-        self.s.close()
+        if self.connected:
+            self.s.close()
         self.connected = False
         pass
-
-    def reset_connection(self):
-        ''' Called at beginning and when peer disconnects. '''
-        if self.s:
-            self.s.close()
-        self.s = socket.socket()
-        self.s.settimeout(30)
-        self.connected = False
-        self.partner = None
-        # TODO how will GUI know that self.connected is False? Poll for
-        # value with after every time local player makes a move?
 
     def recv_update(self):
         pass
 
+class TUI(object):
+    PLAYER_LABELS = {BSTATES['P1']:'X',
+                     BSTATES['P2']:'O',
+                     BSTATES['EMPTY']:'_'}
+
+
+    CONNMSGS = {'connect':'Connecting...',
+                'waiting': 'Connected. Awaiting partner.',
+                'insession': 'Connected. You are',
+                'failed': 'Connection failed.'}
+
+    TURNMSGS = {'partner': "Partner's turn.",
+                'you': 'Your turn.'}
+
+    def __init__(self):
+        self.client = TTTClient()
+        self.tempgame = ttt.TicTacToeGame()
+
+    def parse_input(self, rawcmd):
+        helpmsg = ('\nh - help\n'
+                   'q - quit\n'
+                   'c - reconnect to server\n'
+                   'm - make move with xcoord ycoord, e.g. m 0 0 is top-left\n'
+                   'n - new game\n')
+        cmd = rawcmd.strip().lower()
+        if cmd == 'h':
+            print helpmsg
+        elif cmd == 'q':
+            self.quit()
+        elif cmd == 'c':
+            client.reset_connection()
+        elif cmd == 'n':
+            # if game in progress send forfeit, else do nothing
+            pass
+        elif cmd.startswith('m'):
+            pass
+        else:
+            print 'INVALID COMMAND'
+            print helpmsg
+
+    def quit(self):
+        self.client.end_session()
+        print '\rGoodbye'
+        sys.exit()
+
+    def play(self):
+        while True:
+            self.print_view()
+            self.parse_input(sys.stdin.readline().strip())
+
+    def print_view(self):
+         # connection status - Connected. You are X
+        print '\r', self.CONNMSGS['connect']
+        # stats/score
+        print 'Won, Lost: ', str((0,0))
+        # summary of opponent's move TODO
+        # turn - Your Turn TODO
+        # print self.TURNMSGS['you']
+        # board state
+        print self.tempgame
+        # prompt
+        print '>> ',
+
 
 def main():
-    game = ttt.TicTacToeGame()
-    while True:
-        print game
-        cmd = raw_input(">> ").strip()
-        print cmd
-        if cmd == 'q':
-            return
-
+    TUI().play()
 
 if __name__ == '__main__':
     main()
