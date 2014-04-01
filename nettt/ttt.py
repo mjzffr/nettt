7661ca0ac282bbc01429c71ed7f58a06e39a8a77
Maja Frydrychowicz <mjzffr@gmail.com>
2014-04-01 16:20:19 -0400

    add doc strings

--- nettt/ttt.py
+++ nettt/ttt.py
@@ -6,7 +6,11 @@ GSTATES = {'INPROGRESS':4, 'NOTSTARTED':3, 'P2WON':BSTATES['P2'],
                         'P1WON':BSTATES['P1'], 'DRAW':2}
 
 class TicTacToeGame:
-
+    ''' State consists of two players, board state, game mode (in progress,
+        draw, etc), num games won/lost by each player, last winning coordinates
+        Can generate computer moves; represent board state as a string; record
+        plays actions (move, reset) and their effects.
+    '''
     def __init__(self, size=3, initial_state=None):
         self.SIZE = size
         self.board = [[BSTATES['EMPTY']] * self.SIZE for _ in range(self.SIZE)]
@@ -25,10 +29,16 @@ class TicTacToeGame:
 
     @property
     def board_1d(self):
+        ''' 1D list representing game board1'''
         return [i for row in self.board for i in row]
 
     @property
     def boardstr(self):
+        ''' String representation of game board
+            XO_  is represented as  '1,-1,0;0,-1,1;0,0,1'
+            _OX
+            __X
+        '''
         # more general than necessary (from before this was a property)
         if not TicTacToeGame.is_square(self.board):
             raise ValueError('The grid must be n x n')
@@ -40,12 +50,13 @@ class TicTacToeGame:
 
     def make_random_move(self, player):
         ''' player is 1 or -1 (for X or O)
-            returns the location that was changed
+            in addition to updating board, returns the location that was changed
         '''
         if self.is_over():
             raise Exception("No game in progress. Game over.")
 
-        location = random.choice(self.get_locations(self.board, BSTATES['EMPTY']))
+        location = random.choice(self.get_locations(self.board,
+                                 BSTATES['EMPTY']))
         return self.make_move(player, location)
 
 
@@ -61,10 +72,13 @@ class TicTacToeGame:
     def is_over(self):
         #print "Check %s" % self.mode
         return self.mode <= 2
-        #return self.mode in (GSTATES['XWon'], GSTATES['OWon'], GSTATES['Draw'])
+        #return self.mode in (GSTATES['XWon'], GSTATES['OWon'],GSTATES['Draw'])
 
 
     def reset(self, player):
+        ''' updates game state in response to player asking for a new
+        game
+        '''
         if self.mode == GSTATES['INPROGRESS']:
             self.losses[player] += 1
             self.wins[player * -1] += 1
@@ -75,7 +89,8 @@ class TicTacToeGame:
 
     def make_move(self, player, (row, col)):
         ''' player is 1 or -1 (for X or O)
-            returns location that was changed
+            in addition to updating board, also returns location that was
+            changed
         '''
         if self.is_over():
             raise Exception("No game in progress. Game over.")
@@ -105,6 +120,7 @@ class TicTacToeGame:
         return (row, col)
 
     def update_score(self):
+        ''' increments number of wins/losses for each player'''
         if self.mode == GSTATES['P1WON']:
             self.wins[BSTATES['P1']] += 1
             self.losses[BSTATES['P2']] += 1
@@ -114,35 +130,37 @@ class TicTacToeGame:
 
 
     def update_mode(self):
-            ''' determines whether game is over '''
-            s = self.SIZE
+        ''' updates game mode (in progress, won, etc) according to current
+         state of board '''
+        s = self.SIZE
 
-            # don't check until one player has made > size moves
-            if self.board_1d.count(BSTATES['EMPTY']) > s ** 2 - (s * 2 - 1):
-                self.mode = GSTATES['INPROGRESS']
-                return
+        # don't check until one player has made > size moves
+        if self.board_1d.count(BSTATES['EMPTY']) > s ** 2 - (s * 2 - 1):
+            self.mode = GSTATES['INPROGRESS']
+            return
 
-            self.mode, resultcoords = \
-                TicTacToeGame.decide_mode(self.board)
-            self.ae()
+        self.mode, resultcoords = \
+            TicTacToeGame.decide_mode(self.board)
+        self.ae()
 
-            # <= 2 means draw, xwon or owon
-            if self.mode <= 2:
-                self.lastwincoords = resultcoords
+        # <= 2 means draw, xwon or owon
+        if self.mode <= 2:
+            self.lastwincoords = resultcoords
 
 
     # TODO: testing
     @staticmethod
     def is_winning_line(line):
         ''' return true if line consists of all 'X' or 'O';
-        line is a list of board spaces, like a row or a diagonal'''
+        line is a list of board space values, like a row or a diagonal'''
         return BSTATES['EMPTY'] not in line and all(line[0] == i for i in \
                                                       line)
 
     @staticmethod
     def decide_mode(board):
-        ''' return pair of gstate and set of winning line coords if there is one
-        Assuming board is square!'''
+        ''' return pair of gstate and set of winning line coords if there
+            is one
+        '''
         s = len(board)
 
         for ri,rowvals in enumerate(board):
@@ -161,14 +179,12 @@ class TicTacToeGame:
             if TicTacToeGame.is_winning_line(diagvals):
                 return (diagvals[0], set(diagonal_coords[i]))
 
-        #it's a draw
         board_1d = [i for row in board for i in row]
         if BSTATES['EMPTY'] not in board_1d:
             return (GSTATES['DRAW'], set())
 
         #otherwise
         return (GSTATES['INPROGRESS'], set())
-
 
 
     ''' Helper funcions for compact string representation of game board
@@ -181,19 +197,25 @@ class TicTacToeGame:
 
     @staticmethod
     def str_to_grid(stringrep):
+        ''' returns n by n list given string of the form
+        '1,-1,0;0,-1,1;0,0,1'
+        '''
         return [map(int,i.split(',')) for i in stringrep.split(';')]
 
     @staticmethod
     def str_to_list(stringrep):
+        ''' returns 1d list given string of the form '1,-1,0;0,-1,1;0,0,1'
+        '''
         return TicTacToeGame.grid_to_list(TicTacToeGame.str_to_grid(stringrep))
 
     @staticmethod
     def is_square(grid):
+        ''' true if grid is n x n list '''
         return all([len(grid) == len(i) for i in grid])
 
 
-    # assuming game loop is implemented by UI
     def play(self):
+        '''stub'''
         while self.mode == GSTATES['INPROGRESS']:
             break
             #TODO
@@ -211,4 +233,3 @@ class TicTacToeGame:
                     boardstr += '_'
             boardstr += '\n'
         return boardstr
-
